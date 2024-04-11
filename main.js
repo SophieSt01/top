a8ba453b25d711228aabe8988b571e29b73d4047
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 15:55:58 +0200

    var durch let ersetzt

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var map = L.map('map').setView([45.9369, 7.86675], 13);
+let map = L.map('map').setView([45.9369, 7.86675], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([45.9369, 7.86675]).addTo(map);
+let marker = L.marker([45.9369, 7.86675]).addTo(map);
 marker.bindPopup("<b>Dufourspitze</b><br>4634 m").openPopup();
 
 //marker zeigen
@@ -14,3 +14,11 @@ console.log(marker)
 let age = 10;
 console.log(age)
 */
+
+let absatz = "Hallo Welt!";
+let number = 6;
+
+console.log(`
+    <h3>Test für's "Tutorial"</h3>
+    <p>${absatz}</p>
+    <div id="map"></div>`);
