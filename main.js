a19d584189a71ef13e4d3ad36842b38fab7a84d3
SophieSt01 <sophie.stoffl@uibk.ac.at>
2024-04-11 21:24:53 +0200

    improve marker

--- main.js
+++ main.js
@@ -13,13 +13,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-let marker = L.marker([lat, lng]).addTo(map);
-marker.bindPopup(`
-        <h2>${layer.features.properties.name}</h2>
-        <ul>
-            <li>Breite: ${lat.toFixed(5)}</li>
-            <li>Länge: ${lng.toFixed(5)}</li>
-        `).openPopup();
 
 
 let jsonData = {
